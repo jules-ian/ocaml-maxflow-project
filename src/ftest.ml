7ac7697c80b38ad315dce23ec1ce0c5617a7c99c
jules-ian <julesian.barnavon@gmail.com>
2023-11-24 16:45:31 +0100

    clone_nodes and gmap implemented

--- src/ftest.ml
+++ src/ftest.ml
@@ -1,5 +1,6 @@
 open Gfile
-    
+open Tools
+
 let () =
 
   (* Check the number of command-line arguments *)
@@ -16,10 +17,10 @@ let () =
 
 
   (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
-  
+
   let infile = Sys.argv.(1)
   and outfile = Sys.argv.(4)
-  
+
   (* These command-line arguments are not used for the moment. *)
   and _source = int_of_string Sys.argv.(2)
   and _sink = int_of_string Sys.argv.(3)
@@ -28,8 +29,10 @@ let () =
   (* Open file *)
   let graph = from_file infile in
 
+  (*let graphClone = clone_nodes graph in*)
+  let graphMapArcs = gmap graph (fun _ -> "s") in 
   (* Rewrite the graph that has been read. *)
-  let () = write_file outfile graph in
+  let () = write_file outfile graphMapArcs in
 
-  ()
 
+  ()  
---
+++ src/tools.ml
@@ -0,0 +1,14 @@
+(* Yes, we have to repeat open Graph. *)
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *)
+let clone_nodes (gr:'a graph) = n_fold gr new_node empty_graph
+
+
+let gmap (gr:'a graph) f = e_fold gr (fun acu (arc:'a arc) -> new_arc acu {src = arc.src; tgt = arc.tgt; lbl = (f arc.lbl)} ) (clone_nodes gr)
+(*val e_fold: 'a graph -> ('b -> 'a arc -> 'b) -> 'b -> 'b*)
+
+(**)
+(*
+let add_arc gr id1 id2 a = 
+*)
